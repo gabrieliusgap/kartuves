6b75a49dc566d5273e55f584df6004ccb3ecd8d7
Gabrielius Gaponovas <gabrielius.gaponovas@codeacademylt.onmicrosoft.com>
2021-03-12 16:53:58 +0200

    everything except win or loose

--- kartuves.js
+++ kartuves.js
@@ -8,13 +8,18 @@ let letter = document.getElementsByClassName("letter")
 startGameButton.addEventListener("click", startGameFun)
 
 let setWord =""
+let setWordArr=[]
 let mistakeCounter = 0
+let indexOfGuessed=[]
 
 function startGameFun(){
+
     startGameButton.removeEventListener("click", startGameFun)
 
     setWord = secretWord.value.toUpperCase()
     console.log(setWord)
+    setWordArr=setWord.split()
+    console.log(setWordArr)
     for (let i = 0; i < setWord.length; i++) {
         wordUnknownYet.innerHTML+=`
         <div id="i" class="unknownYetLetter">_</div>
@@ -27,13 +32,31 @@ function startGameFun(){
 function checkLetterFun(event){
     console.log(event.target.innerText)
     event.target.removeEventListener("click", checkLetterFun)
+    let letterGuessed =""
+
     if(setWord.includes(event.target.innerText)){
+        letterGuessed = event.target.innerText
+
         console.log(event.target.innerText)
         event.target.style.backgroundColor = "green"
+
+
+        for (let i = 0; i < setWord.length; i++) {
+            if(setWord[i]===letterGuessed){
+               indexOfGuessed.push(i)
+            }
+        }
     } else {
         console.log("ner tokios")
         event.target.style.backgroundColor = "red"
         mistakeCounter+= 1
         mistakes.innerText = mistakeCounter
     }
+    console.log(indexOfGuessed)
+    for (let i = 0; i <indexOfGuessed.length ; i++) {
+        unknownYetLetter[indexOfGuessed[i]].innerText = event.target.innerText
+    }
+    indexOfGuessed=[]
+// checkIfWinOrLose()
+
 }
