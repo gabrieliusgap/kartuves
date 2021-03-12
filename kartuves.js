2e89033d6ee56e0f53f582d4c15561389daa6323
Gabrielius Gaponovas <gabrielius.gaponovas@codeacademylt.onmicrosoft.com>
2021-03-12 16:23:57 +0200

    alphabet, mistake counter working

--- kartuves.js
+++ kartuves.js
@@ -3,17 +3,37 @@ let startGameButton = document.getElementById("startGameButton")
 let mistakes = document.getElementById("mistakes")
 let wordUnknownYet = document.getElementById("wordUnknownYet")
 let unknownYetLetter = document.getElementsByClassName("unknownYetLetter")
-let letter = document.getElementsByClassName("unknownYetLetter")
+let letter = document.getElementsByClassName("letter")
 
 startGameButton.addEventListener("click", startGameFun)
 
-
 let setWord =""
+let mistakeCounter = 0
+
 function startGameFun(){
-    setWord = secretWord.value
+    startGameButton.removeEventListener("click", startGameFun)
+
+    setWord = secretWord.value.toUpperCase()
+    console.log(setWord)
     for (let i = 0; i < setWord.length; i++) {
-        wordUnknownYet+=`
-        <div id="${i-1}" class="unknownYetLetter">_</div>
+        wordUnknownYet.innerHTML+=`
+        <div id="i" class="unknownYetLetter">_</div>
         `
+    }
+    for (let i = 0; i < 25; i++) {
+        letter[i].addEventListener("click", checkLetterFun)
+    }
+}
+function checkLetterFun(event){
+    console.log(event.target.innerText)
+    event.target.removeEventListener("click", checkLetterFun)
+    if(setWord.includes(event.target.innerText)){
+        console.log(event.target.innerText)
+        event.target.style.backgroundColor = "green"
+    } else {
+        console.log("ner tokios")
+        event.target.style.backgroundColor = "red"
+        mistakeCounter+= 1
+        mistakes.innerText = mistakeCounter
     }
 }
